d31bfcc2588e669acd9545cf74db5b1baf1bca56
azizulhooq46-pixel <azizulhooq46@gmail.com>
2026-09-04 15:54:45 +0600

    বট.py

--- বট.py
+++ বট.py
@@ -20,7 +20,18 @@ def run():
 def keep_alive():
   t = Thread(target=run)
   t.start()
+app = Flask('')
 
+@app.route('/')
+def home():
+    return "I'm alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 # Intents
 blah = discord.Intents.default()
 blah.message_content = True
